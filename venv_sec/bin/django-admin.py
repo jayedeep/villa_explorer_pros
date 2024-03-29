25d9d5799e8dd6d4e08261f78b33856a36876d7a
jaydeep <jaydeeptalaviya8@gmail.com>
2024-03-29 22:11:10 +0530

    added correct requirement file

---
+++ venv_sec/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/jay/New Volume/development_2022_2023/btre_web_django-master/venv_sec/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
