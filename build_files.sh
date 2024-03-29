1b64e08c432809e7670701473d701ca5db2ae6a7
jaydeep <jaydeeptalaviya8@gmail.com>
2024-03-29 21:52:14 +0530

    added change into bash file for vercel 2

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-# Install pip (if needed)
-if ! command -v pip &> /dev/null; then
-  # Replace 'python3' with the appropriate command for your Python version
-  get-pip.py https://pypi.org/project/pip/
-fi
-
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
+
